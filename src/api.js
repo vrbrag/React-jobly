d14abc1284529aee4fe30efe6c31564d15750a7f
Vanessa Bragdon <vrbragdon@gmail.com>
2023-01-10 16:24:43 -0800

    api helpers

--- src/api.js
+++ src/api.js
@@ -22,8 +22,8 @@ class JoblyApi {
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
     const params = (method === "get")
-        ? data
-        : {};
+      ? data
+      : {};
 
     try {
       return (await axios({ url, method, data, params, headers })).data;
@@ -35,18 +35,59 @@ class JoblyApi {
   }
 
   // Individual API routes
-
+  // -----------------------------------------Companies 
   /** Get details on a company by handle. */
 
   static async getCompany(handle) {
     let res = await this.request(`companies/${handle}`);
     return res.company;
   }
+  /** Get all companies; filter: minEmployees, maxEmployees, name */
+  static async getAllCompanies(filter = {}) {
+    let res = await this.request(`companies`, filter);
+    return res.companies;
+  }
 
-  // obviously, you'll add a lot here ...
+  // -----------------------------------------Jobs
+  /** Get details on a job by handle. */
+  static async getJob(id) {
+    let res = await this.request(`jobs/${id}`);
+    return res.job;
+  }
+  /** Get all jobs; filter: minSalary, hasEquity, title */
+  static async getAllJobs(filter = {}) {
+    let res = await this.request(`jobs`, filter);
+    return res.jobs;
+  }
+
+  // -----------------------------------------User
+  /** Get current user */
+  static async getCurrentUser(username) {
+    let res = await this.request(`users/${username}`)
+    return res.user
+  }
+  /** Signup for site and store token on class and return token */
+  static async signup(userData) {
+    let res = await this.request(`auth/register`, userData, "post")
+    return res.token
+  }
+  /** Login user and store token on class and return token */
+  static async login(userData) {
+    let res = await this.request(`auth/authenticate`, userData, "post")
+    return res.token
+  }
+  /** Save/update user information */
+  static async updateUser(username, data) {
+    let res = await this.request(`users/${username}`, data, "patch")
+    return res.user
+  }
+  /** Get all jobs; filter: minSalary, hasEquity, title */
+  static async apply(username, id) {
+    await this.request(`/users/${username}/jobs/${id}`, {}, "post")
+  }
 }
 
 // for now, put token ("testuser" / "password" on class)
 JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+  "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
+  "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
