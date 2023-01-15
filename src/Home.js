80996107dca863503d0d3cc203515c65328e0f2d
Vanessa Bragdon <vrbragdon@gmail.com>
2023-01-14 22:29:27 -0800

    home page

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,29 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import UserContext from './UserContext';
+import { Link } from 'react-router-dom'
 
 function Home() {
+   const { currentUser } = useContext(UserContext)
+
+   console.debug("Homepage", "currentUser=", currentUser)
 
    return (
       <div>
-         <h2>Jobly</h2>
-         <p>Let your job search begin here!</p>
-         <h3>Welcome back, user!</h3>
+         <div>
+            <h1>Jobly</h1>
+            <p>Let your job search begin here!</p>
+            {currentUser ?
+               <h2>Welcome Back, {currentUser.firstName || currentUser.username}! </h2>
+               : (
+                  <p>
+                     <Link className="btn btn-primary font-weight-bold mr-3" to="/login">Log in
+                     </Link>
+                     <Link className="btn btn-primary font-weight-bold mr-3" to="/signup">Sign up
+                     </Link>
+                  </p>
+               )
+            }
+         </div>
       </div>
    )
 };
